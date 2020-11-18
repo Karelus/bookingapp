cb9a4cf666fef02e7e902ca86343927ecd19a150
Karelus <karel.haapasalo@hotmail.com>
2020-11-18 14:01:58 +0200

    modified app, added models

--- app.py
+++ app.py
@@ -1,11 +1,103 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from http import HTTPStatus
 
 app = Flask(__name__)
 
+reviews = [
+    {
+        'id': 1,
+        'author': 'Karel',
+        'content': 'This was a very good movie.'
+    },
+    {
+        'id': 2,
+        'author': 'Kappe',
+        'content': 'Not that good tbh.'
+    }
+]
 
-@app.route("/")
-def test():
-    return "Testi"
+movies = [
+    {
+        'id': 1,
+        'name': 'Matrix',
+        'description': 'Guy takes a red pill.'
+    },
+    {
+        'id': 2,
+        'name': 'Fight Club',
+        'description': 'Guy establishes an underground fight club.'
+    }
+]
+
+
+# get methods
+@app.route('/reviews', methods=['GET'])
+def get_reviews():
+    return jsonify({'data': reviews})
+
+
+@app.route('/reviews/<int:review_id>', methods=['GET'])
+def get_review(review_id):
+    review = next((review for review in reviews if review['id'] == review_id), None)
+
+    if review:
+        return jsonify(review)
+
+    return jsonify({'message': 'review not found'}), HTTPStatus.NOT_FOUND
+
+
+@app.route('/movies', methods=['GET'])
+def get_movies():
+    return jsonify({'data': movies})
+
+
+@app.route('/movies/<int:movie_id>', methods=['GET'])
+def get_movie(movie_id):
+    movie = next((movie for movie in movies if movie['id'] == movie_id), None)
+
+    if movie:
+        return jsonify(movie)
+
+    return jsonify({'message': 'movie not found'}), HTTPStatus.NOT_FOUND
+
+
+# post methods
+@app.route('/reviews', methods=['POST'])
+def create_review():
+    data = request.get_json()
+
+    author = data.get('author')
+    content = data.get('content')
+
+    review = {
+        'id': len(reviews) + 1,
+        'author': author,
+        'content': content
+    }
+
+    reviews.append(review)
+
+    return jsonify(review), HTTPStatus.CREATED
+
+
+# put methods
+@app.route('/reviews/<int:review_id>', methods=['PUT'])
+def update_review(review_id):
+    review = next((review for review in reviews if review['id'] == review_id), None)
+
+    if not review:
+        return jsonify({'message': 'review not found'}), HTTPStatus.NOT_FOUND
+
+    data = request.get_json()
+
+    review.update(
+        {
+            'author': data.get('author'),
+            'content': data.get('content')
+        }
+    )
+
+    return jsonify(review)
 
 
 if __name__ == "__main__":
---
+++ models/movie.py
@@ -0,0 +1,25 @@
+from extensions import db
+
+
+# for making the movie table in database
+class Movie(db.Model):
+    __tablename__ = 'movie'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+    description = db.Column(db.String(1000))
+    director = db.Column(db.String(50), nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+    age_rating = db.Column(db.Integer, nullable=False)
+    added_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+
+    @classmethod
+    def get_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
