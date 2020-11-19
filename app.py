962c13e1a8f80db577fd990cb3e811d8fbf2f180
Karelus <karel.haapasalo@hotmail.com>
2020-11-19 12:43:14 +0200

    added user functionality and database creation code

--- app.py
+++ app.py
@@ -1,12 +1,41 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_restful import Api
 
+from Config import Config
+from extensions import db
+from resources.user import UserListResource
 from resources.review import ReviewListResource, ReviewResource, ReviewPublishResource
 from resources.movie import MovieListResource, MovieResource
 
 
-app = Flask(__name__)
-api = Api(app)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    register_extensions(app)
+    register_resources(app)
+
+    return app
+
+
+def register_extensions(app):
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+
+def register_resources(app):
+    api = Api(app)
+
+    api.add_resource(UserListResource, '/users')
+
+    api.add_resource(ReviewListResource, '/reviews')
+    api.add_resource(ReviewResource, '/reviews/<int:review_id>')
+    api.add_resource(ReviewPublishResource, '/reviews/<int:review_id>/publish')
+
+    api.add_resource(MovieListResource, '/movies')
+    api.add_resource(MovieResource, '/movies/<int:movie_id>')
+
 
 reviews = [
     {
@@ -34,84 +63,9 @@ movies = [
     }
 ]
 
-api.add_resource(ReviewListResource, '/reviews')
-api.add_resource(ReviewResource, '/reviews/<int:review_id>')
-api.add_resource(ReviewPublishResource, '/reviews/<int:review_id>/publish')
-
-api.add_resource(MovieListResource, '/movies')
-api.add_resource(MovieResource, '/movies/<int:movie_id>')
-
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
-
-"""
-# get methods
-@app.route('/reviews', methods=['GET'])
-def get_reviews():
-    return jsonify({'data': reviews})
+    app = create_app()
+    app.run()
 
 
-@app.route('/reviews/<int:review_id>', methods=['GET'])
-def get_review(review_id):
-    review = next((review for review in reviews if review['id'] == review_id), None)
-
-    if review:
-        return jsonify(review)
-
-    return jsonify({'message': 'review not found'}), HTTPStatus.NOT_FOUND
-
-
-@app.route('/movies', methods=['GET'])
-def get_movies():
-    return jsonify({'data': movies})
-
-
-@app.route('/movies/<int:movie_id>', methods=['GET'])
-def get_movie(movie_id):
-    movie = next((movie for movie in movies if movie['id'] == movie_id), None)
-
-    if movie:
-        return jsonify(movie)
-
-    return jsonify({'message': 'movie not found'}), HTTPStatus.NOT_FOUND
-
-
-# post methods
-@app.route('/reviews', methods=['POST'])
-def create_review():
-    data = request.get_json()
-
-    author = data.get('author')
-    content = data.get('content')
-
-    review = {
-        'id': len(reviews) + 1,
-        'author': author,
-        'content': content
-    }
-
-    reviews.append(review)
-
-    return jsonify(review), HTTPStatus.CREATED
-
-
-# put methods
-@app.route('/reviews/<int:review_id>', methods=['PUT'])
-def update_review(review_id):
-    review = next((review for review in reviews if review['id'] == review_id), None)
-
-    if not review:
-        return jsonify({'message': 'review not found'}), HTTPStatus.NOT_FOUND
-
-    data = request.get_json()
-
-    review.update(
-        {
-            'author': data.get('author'),
-            'content': data.get('content')
-        }
-    )
-
-    return jsonify(review)
-"""
--- models/review.py
+++ models/review.py
@@ -1,26 +1,4 @@
-"""
 from extensions import db
-
-
-# for making the review table in database
-class Review(db.Model):
-    __tablename__ = 'review'
-
-    id = db.Column(db.Integer, primary_key=True)
-    movie_id = db.Column(db.Integer, nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    content = db.Column(db.String(1000))
-    added_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
-
-    @classmethod
-    def get_by_id(cls, movie_id):
-        return cls.query.filter_by(movie_id=movie_id).first()
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-"""
 
 review_list = []
 
@@ -33,19 +11,25 @@ def get_last_id():
     return last_review.id + 1
 
 
-class Review:
-    def __init__(self, movie_id, author, content):
-        self.id = get_last_id()
-        self.movie_id = movie_id
-        self.author = author
-        self.content = content
-        self.is_publish = False
+# for making the review table in database
+class Review(db.Model):
+    __tablename__ = 'review'
 
-    @property
-    def data(self):
-        return {
-            'id': self.id,
-            'movie_id': self.movie_id,
-            'author': self.author,
-            'content': self.content
-        }
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(1000))
+    added_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+"""
+    @classmethod
+    def get_by_id(cls, movie_id):
+        return cls.query.filter_by(movie_id=movie_id).first()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+"""
